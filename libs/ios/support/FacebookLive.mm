7f8f4bc24b6e198b90fae1f9c15aa04c0227f4e5
wuzhiming <wuzhiming.chukong@gmail.com>
2020-03-13 17:47:08 +0800

    fix bug when code is not run at renderer thread

--- libs/ios/support/FacebookLive.mm
+++ libs/ios/support/FacebookLive.mm
@@ -1,8 +1,11 @@
 #import "FacebookLive.h"
+#include "platform/CCApplication.h"
 #import <Bolts/Bolts.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLiveStreamingKit/FBSDKLiveStreamingKit.h>
 #include "cocos/scripting/js-bindings/jswrapper/SeApi.h"
+#include "base/CCScheduler.h"
+#include <string>
 @interface FacebookLive ()<FBSDKLiveStreamingObserver>
 @property(nullable,retain) FBSDKLiveStreamingConfig* _liveStreamingConfig;
 @end
@@ -53,7 +56,10 @@ static FBSDKLiveStreamingConfig *_liveStreamingConfig;
 - (void)onLiveStreamingError:(NSNotification *)notification {
     FBSDKLiveStreamingStatus *status = (FBSDKLiveStreamingStatus *)notification.userInfo[FBSDKLiveStreamingStatusKey];
     NSString *execStr = [NSString stringWithFormat:@"fb.liveStream._live_error_received('%lu')",(unsigned long)status.code];
-    se::ScriptEngine::getInstance()->evalString([execStr UTF8String]);
+    std::string str = [execStr UTF8String];
+    cocos2d::Application::getInstance()->getScheduler()->performFunctionInCocosThread([=](){
+        se::ScriptEngine::getInstance()->evalString(str.c_str());
+    });
 }
 
 - (void)onLiveStreamingStarted:(NSNotification *)notification {
@@ -63,8 +69,10 @@ static FBSDKLiveStreamingConfig *_liveStreamingConfig;
 - (void)onLiveStreamingStatus:(NSNotification *)notification {
     FBSDKLiveStreamingStatus *status = (FBSDKLiveStreamingStatus *)notification.userInfo[FBSDKLiveStreamingStatusKey];
     NSString *execStr = [NSString stringWithFormat:@"fb.liveStream._live_status_changed('%lu')",(unsigned long)status.code];
-    se::ScriptEngine::getInstance()->evalString([execStr UTF8String]);
-
+    std::string str = [execStr UTF8String];
+    cocos2d::Application::getInstance()->getScheduler()->performFunctionInCocosThread([=](){
+        se::ScriptEngine::getInstance()->evalString(str.c_str());
+    });
 }
 
 +(FacebookLive *) getInstance{
